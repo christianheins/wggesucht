3754b18675006d1c8e0699c830b1274eb1c8dc3b
christianheins <christian_heins@hotmail.com>
2023-04-21 20:05:21 +0200

    Testing the commit command

--- wggesucht.py
+++ wggesucht.py
@@ -934,7 +934,7 @@ def main():
             st.altair_chart(chart.interactive(), use_container_width=True)
 
             chart = alt.Chart(df_concat).mark_point().encode(
-                x=alt.X('Neighbourhood:Q', title='Size', axis=alt.Axis(tickCount=5)),
+                x=alt.X('Neighbourhood:N', title='Size', axis=alt.Axis(tickCount=5)),
                 y=alt.Y('Miete:Q', title='Rent', axis=alt.Axis(tickCount=5)),
                 tooltip=['Neighbourhood', 'Miete']
             )
@@ -951,7 +951,7 @@ def main():
             st.altair_chart(chart.interactive(), use_container_width=True)
 
             chart = alt.Chart(df_concat).mark_point().encode(
-                x=alt.X('Neighbourhood:Q', title='Size', axis=alt.Axis(tickCount=5)),
+                x=alt.X('Neighbourhood:N', title='Size', axis=alt.Axis(tickCount=5)),
                 y=alt.Y('EUR / SQM:Q', title='Rent per SQM', axis=alt.Axis(tickCount=5)),
                 tooltip=['Neighbourhood', 'EUR / SQM']
             )
