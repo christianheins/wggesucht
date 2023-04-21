677e03147058729c053463de5c4cb89bc248a9db
christianheins <christian_heins@hotmail.com>
2023-04-21 19:11:30 +0200

    Testing the commit command

--- wggesucht.py
+++ wggesucht.py
@@ -954,7 +954,7 @@ def main():
         df_timeline_pivotedby_dataframedate = df_timeline.pivot_table(index="Dataframe Date", aggfunc={"Miete":["mean","sum"]}).reset_index()
         st.write(df_timeline_pivotedby_dataframedate)
         st.write(df_timeline_pivotedby_dataframedate.columns)
-        miete_average = str(df_timeline_pivotedby_dataframedate.columns.values)
+        miete_average = df_timeline_pivotedby_dataframedate.columns[1].values
         st.write(miete_average)
         miete_sum = str(df_timeline_pivotedby_dataframedate.columns[2])
         st.write(miete_sum)
