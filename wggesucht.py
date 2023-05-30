b1576ba9fd8ea4b51f77b58bccb287330a78e70d
christianheins <christian_heins@hotmail.com>
2023-05-30 13:44:48 +0200

    Testing the commit command

--- wggesucht.py
+++ wggesucht.py
@@ -459,7 +459,7 @@ def main():
                 chart = alt.Chart(df_statistics.reset_index()).mark_boxplot().encode(
                     y='Miete:Q'
                 ).properties(
-                    height=500,
+                    height=300,
                     width=30
                 )
                 st.altair_chart(chart)
