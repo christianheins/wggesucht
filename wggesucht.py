f87c1ae39c5dff3fe3692d4cf8ed63b134b2365f
christianheins <christian_heins@hotmail.com>
2023-05-30 13:38:33 +0200

    Testing the commit command

--- wggesucht.py
+++ wggesucht.py
@@ -456,8 +456,7 @@ def main():
                 st.metric("75% of the leases are up to", value="{:,.0f} €".format(df_statistics.loc["75%"]["Miete"]))
                 st.metric("Max rent", value="{:,.0f} €".format(df_concat["Miete"].max()))
                 chart = alt.Chart(df_statistics.reset_index()).mark_boxplot().encode(
-                    x='index:N',
-                    y='Miete:Q'
+                    y='Miete :Q'
                 )
                 st.altair_chart(chart)
             with col2:
