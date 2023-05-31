4b04c6784bff61e71eaeab4203c28aa94ba63403
christianheins <christian_heins@hotmail.com>
2023-05-31 08:53:26 +0200

    Testing the commit command

--- wggesucht.py
+++ wggesucht.py
@@ -640,8 +640,8 @@ def main():
             st.altair_chart(chart.interactive(), use_container_width=True)
 
             chart = alt.Chart(df_concat).mark_point(color="orange").encode(
-                x=alt.X('Lease term:Q', title='Neighbourhood', axis=alt.Axis(tickCount=5)),
-                y=alt.Y('EUR / SQM:Q', title='Rent', axis=alt.Axis(tickCount=5)),
+                x=alt.X('Lease term:Q', title='Lease term', axis=alt.Axis(tickCount=5)),
+                y=alt.Y('EUR / SQM:Q', title='EURO per SQM', axis=alt.Axis(tickCount=5)),
                 tooltip=['Lease term', 'EUR / SQM']
             )
             # show the chart
