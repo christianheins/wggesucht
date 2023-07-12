2522f9fb2016b4ee4e845e9e4be92c9ae67267ca
christianheins <christian_heins@hotmail.com>
2023-07-12 23:28:52 +0200

    Testing the commit command

--- wggesucht.py
+++ wggesucht.py
@@ -757,7 +757,7 @@ def main():
         df_2023_07.rename(columns={"index":"Data ID", "Latitude":"lat", "Longitude":"lon"}, inplace=True)
         df_2023_07.drop(columns=["Unnamed: 0.1", "Unnamed: 0"], inplace=True)
         df_2023_07.reset_index(drop=True, inplace=True)
-        df_2023_07["Pure Rent"] = df_2023_06["Price"]
+        df_2023_07["Pure Rent"] = df_2023_07["Price"]
         df_2023_07["Dataframe Date"] = "20230712"
 
 
@@ -769,7 +769,7 @@ def main():
         st.write(df_2023_07)
 
 
-        df_timeline = pd.concat([df_2023_06, df_2023_07])
+        df_timeline = pd.concat([df_2023_05, df_2023_06, df_2023_07])
         st.write(df_timeline)
         st.write(len(df_timeline))
         df_timeline_pivotedby_dataframedate = df_timeline.pivot_table(index="Dataframe Date", aggfunc={"Pure Rent":["mean","sum"]}).reset_index()
