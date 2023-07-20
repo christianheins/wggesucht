e2ccc81b2f8e845b61da28b18bdc939032e5df3a
christianheins <christian_heins@hotmail.com>
2023-07-20 21:33:27 +0200

    Testing the commit command

--- wggesucht.py
+++ wggesucht.py
@@ -427,8 +427,8 @@ def main():
                 #Tell the client to select a channel and include the specified text.
                 client.chat_postMessage(channel='#special-projects', text=f"Message from an user:\n\n{text}")
                 print("Sending slack message")
-                button = st.button("Send message")
-                if button:
+                button2 = st.button("Send message")
+                if button2:
                     sendslack()
 
 
