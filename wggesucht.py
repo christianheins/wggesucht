a44a505fb3a6a7603d81145e8d097fd9b8c8da5f
christianheins <christian_heins@hotmail.com>
2023-07-21 11:30:20 +0200

    Testing the commit command

--- wggesucht.py
+++ wggesucht.py
@@ -420,14 +420,14 @@ def main():
             st.markdown(f"<li style='text-align: left; color: grey; font-size: 12px;'>This web applications is capturing a snapshot of the last months entries as of the date the csv file was lastly refreshed from here: 'https://www.wg-gesucht.de/1-zimmer-wohnungen-und-wohnungen-in-Berlin.8.1+2.0.0.html?pagination=1&pu='</li>", unsafe_allow_html=True)
             st.markdown(f"<li style='text-align: left; color: grey; font-size: 12px;'>Please use as a guide for only the WG Gesucht portal, this data is not completly representative. It's just an example of the powerful features Steramlit has to offer. Logos and images are WG Gesuchts property and not mine.</li>", unsafe_allow_html=True)
 
-            text = st.text_input("Send me a message on slack")
-            def sendslack():
-                #Create a slack client and define todays date or moment date
-                client = slack.WebClient(token=st.secrets.slack_bot_token)
+        text = st.text_input("Send me a message on slack")
+        def sendslack():
+            #Create a slack client and define todays date or moment date
+            client = slack.WebClient(token=st.secrets.slack_bot_token)
 
-                #Tell the client to select a channel and include the specified text.
-                client.chat_postMessage(channel='#special-projects', text=f"Message from an user:\n\n{text}")
-                print("Sending slack message")
+            #Tell the client to select a channel and include the specified text.
+            client.chat_postMessage(channel='#special-projects', text=f"Message from an user:\n\n{text}")
+            print("Sending slack message")
         button2 = st.button("Send message")
         if button2:
             sendslack()
