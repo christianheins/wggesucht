144e964dc1e159d5036cded5019dc90fbccb13cf
christianheins <christian_heins@hotmail.com>
2023-07-23 18:36:36 +0200

    Testing the commit command

--- wggesucht.py
+++ wggesucht.py
@@ -457,7 +457,7 @@ def main():
             st.dataframe(df_concat[["Data ID", "Eintrag", "City", "Neighbourhood", "Address", "Miete" , "Größe", "EUR / SQM", "Deposit", "frei ab", "frei bis", "frei bis (Year - Month)", "DataFrame", "Latitude", "Longitude", "Link"]],
                          column_config={
                             "Data ID": st.column_config.NumberColumn(format="%d"),
-                            "Miete": st.column_config.NumberColumn(format="%d €"),
+                            "Miete": st.column_config.NumberColumn(format="%d,00 €"),
                             "EUR / SQM": st.column_config.NumberColumn(format="%d €"),
                             "Link": st.column_config.LinkColumn("Link")
                             }
