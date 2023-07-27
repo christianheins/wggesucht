49ccae550a691a85605290cb122bdd4c0ff42b3f
christianheins <christian_heins@hotmail.com>
2023-07-27 17:17:37 +0200

    Testing the commit command

--- wggesucht.py
+++ wggesucht.py
@@ -424,6 +424,7 @@ def main():
         st.markdown("<h6 style='text-align: left; color: red;'>Instructions</h6>", unsafe_allow_html=True)
         st.markdown(f"<li style='text-align: left; color: grey; font-size: 12px;'>This web applications is capturing a snapshot of the last months entries as of the date the csv file was lastly refreshed from here: 'https://www.wg-gesucht.de/1-zimmer-wohnungen-und-wohnungen-in-Berlin.8.1+2.0.0.html?pagination=1&pu='</li>", unsafe_allow_html=True)
         st.markdown(f"<li style='text-align: left; color: grey; font-size: 12px;'>Please use as a guide for only the WG Gesucht portal, this data is not completly representative. It's just an example of the powerful features Steramlit has to offer. Logos and images are WG Gesuchts property and not mine.</li>", unsafe_allow_html=True)
+    st.markdown("---")
 
     col1, col2 = st.columns([0.2, 0.8])
     with col1:
