7cc389ed694cd971ef66ebdbe8e2cb0c8ec115dd
christianheins <christian_heins@hotmail.com>
2023-07-27 18:53:23 +0200

    Testing the commit command

--- wggesucht.py
+++ wggesucht.py
@@ -468,7 +468,7 @@ def main():
 
         with col2:
             st.write("Another filter")
-            slider_europersqm = st.slider("Chosse an euro per sqm range", 0, 100, (25, 75))
+            slider_europersqm = st.slider("Chosse an euro per sqm range", table_df["EUR / SQM"].min(), table_df["EUR / SQM"].max(), (0, table_df["EUR / SQM"].max))
             st.write("Your selection:", slider_europersqm)
             st.write(slider_europersqm[0])
             st.write(slider_europersqm[1])
