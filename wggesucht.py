8bde7603ff70751b50a5de00d365a1ed7ff49b2c
christianheins <christian_heins@hotmail.com>
2023-07-27 11:13:27 +0200

    Testing the commit command

--- wggesucht.py
+++ wggesucht.py
@@ -396,10 +396,11 @@ def main():
 
 
     df_concat = pd.read_csv(nameofdataframe)
+    df_concat["Link"] = "https://"+df_concat["Link"]
+
     #df_concat.drop(columns=["Unnamed: 0.1", "Unnamed: 0"], inplace=True)
     #df_concat.rename(columns = {"Posting Date":"Eintrag", "Price":"Miete", "Size":"Größe","Date From":"frei ab", "Date To":"frei bis", "Location":"Stadtteil", "Date To (Year - Month)":"frei bis (Year - Month)"}, inplace=True)
     df_immowelt = pd.read_csv("df_immowelt_rebuilt.csv")
-    df_immowelt["Link"] = "https://"+df_immowelt["Link"]
     df_concat = pd.concat([df_concat, df_immowelt])
     st.write(df_concat)
 
