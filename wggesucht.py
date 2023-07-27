c96dda486dfb6342f572e4d2a1157cf30e632f2a
christianheins <christian_heins@hotmail.com>
2023-07-27 17:37:50 +0200

    Testing the commit command

--- wggesucht.py
+++ wggesucht.py
@@ -457,6 +457,18 @@ def main():
 
         col1, col2, col3 = st.columns([0.3, 0.3, 0.3])
         with col1:
+            # Create a custom CSS style for the placeholder
+            placeholder_style = """
+            <style>
+            div[role="listbox"] .Select__placeholder {
+              color: #9e9e9e;
+            }
+            </style>
+            """
+
+            # Display the custom CSS style
+            st.markdown(placeholder_style, unsafe_allow_html=True)
+
             multiselect_neighbourhoods = st.multiselect("Choose a neighbourhood", options=df_concat["Neighbourhood"].unique().tolist(), help="Please do not leave empty")
             st.write("You selected:", multiselect_neighbourhoods)
             table_df = df_concat[df_concat["Neighbourhood"].isin(multiselect_neighbourhoods)]
