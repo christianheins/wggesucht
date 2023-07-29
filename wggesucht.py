1830e1a8c18e8bf45dbcd90687054dd49eaed677
christianheins <christian_heins@hotmail.com>
2023-07-29 10:39:51 +0200

    Testing the commit command

--- wggesucht.py
+++ wggesucht.py
@@ -806,7 +806,9 @@ def main():
 
         col1, col2 = st.columns([0.4, 0.6])
         with col1:
-            st.dataframe(df_concat.pivot_table(index="Neighbourhood", aggfunc={"Pure Rent":["mean","sum"], "Size":["mean","sum"]}), use_container_width=True)
+            df_neighbourhoods = df_concat.pivot_table(index="Neighbourhood", aggfunc={"Pure Rent":["mean","sum"], "Size":["mean","sum"]})
+            df_neighbourhoods.columns = [''.join(col).strip() for col in df_neighbourhoods.columns.values]
+            st.dataframe(df_neighbourhoods, use_container_width=True)
         with col2:
             st.map(df_concat)
 
